209f12202bfbed886f7f952dbb0217a51c9df3bd
panthurr0 <olapopaola@mail.ru>
2024-08-11 11:20:23 +1200

    reformat code w black

--- config/urls.py
+++ config/urls.py
@@ -18,20 +18,11 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     # swagger
     path(
         "swagger/",
-        schema_view.with_ui(
-            "swagger",
-            cache_timeout=0),
-        name="schema-swagger-ui"
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
     ),
-    path(
-        "redoc/",
-        schema_view.with_ui(
-            "redoc",
-            cache_timeout=0),
-        name="schema-redoc"
-    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
