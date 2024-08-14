572d19cf0d5ba57b63f29d8c5908f221f21d5e79
panthurr0 <olapopaola@mail.ru>
2024-08-14 15:40:50 +1200

    excluded fields in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,7 @@ from users.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        exclude = ['token', 'role']
 
 
 class UserResetPasswordSerializer(ModelSerializer):
